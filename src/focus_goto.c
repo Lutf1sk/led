c0cc8262ef5407c98a4dfba89a8e34e38561a647
Alex Edin <lutfisk@lutfisk.net>
2021-08-04 19:34:33 +0200

    Print desciption of goto command while typing

--- src/focus_goto.c
+++ src/focus_goto.c
@@ -16,6 +16,46 @@ focus_t focus_goto = { draw_goto, NULL, input_goto };
 static char input_buf[64];
 static lstr_t input = LSTR(input_buf, 0);
 
+static
+usz interp_str(editor_t* ed, lstr_t str, b8* out_sync_selection, char* out_dir) {
+	b8 sync_selection = 1;
+	char dir = 0;
+	
+	usz line = 0;
+
+	for (usz i = 0; i < input.len; ++i) {
+		char c = input.str[i];
+		switch (c) {
+		case 's':
+			sync_selection = 0;
+			break;
+			
+		case 'u': case'd':
+			dir = c;
+			break;
+		
+		case 'e':
+			line = ed->doc.line_count + 1;
+			break;
+		
+		case 'b':
+			line = 1;
+			break;
+			
+		default:
+			if (is_digit(c)) {
+				line *= 10;
+				line += c - '0';
+			}
+			break;
+		}
+	}
+	
+	*out_sync_selection = sync_selection;
+	*out_dir = dir;
+	return line;
+}
+
 void goto_line(void) {
 	input.len = 0;
 	focus = focus_goto;
@@ -28,8 +68,24 @@ void draw_goto(global_t* ed_global, void* win_, void* args) {
 	isz height = ed_global->height;
 	
 	wattr_set(win, 0, PAIR_BROWSE_FILES_INPUT, NULL);
-	mvwprintw(win, height - 1, 0, " %.*s", (int)input.len, input.str);
+	mvwprintw(win, height - 2, 0, " %.*s", (int)input.len, input.str);
 	wcursyncup(win);
+	waddnch(win, width - getcurx(win), ' ');
+	
+	wattr_set(win, 0, PAIR_BROWSE_FILES_SEL, NULL);
+	b8 sync_selection; char dir; usz line;
+	line = interp_str(*ed_global->ed, input, &sync_selection, &dir);
+	
+	mvwaddstr(win, height - 1, 0, sync_selection ? " Jump" : " Select");
+	if (line) {
+		if (dir == 0)
+			wprintw(win, " to line %zu", line);
+		else if (dir == 'u')
+			wprintw(win, " UP %zu lines", line);
+		else if (dir == 'd')
+			wprintw(win, " DOWN %zu lines", line);
+	}
+	
 	waddnch(win, width - getcurx(win), ' ');
 }
 
@@ -62,38 +118,8 @@ void input_goto(global_t* ed_global, int c) {
 		break;
 
 	case KEY_ENTER: case '\n': {
-		b8 sync_selection = 1;
-		char dir = 0;
-		
-		u64 line = 0;
-		
-		for (usz i = 0; i < input.len; ++i) {
-			char c = input.str[i];
-			switch (c) {
-			case 's':
-				sync_selection = 0;
-				break;
-				
-			case 'u': case'd':
-				dir = c;
-				break;
-			
-			case 'e':
-				line = ed->doc.line_count + 1;
-				break;
-			
-			case 'b':
-				line = 1;
-				break;
-				
-			default:
-				if (is_digit(c)) {
-					line *= 10;
-					line += c - '0';
-				}
-				break;
-			}
-		}
+		b8 sync_selection; char dir; usz line;
+		line = interp_str(ed, input, &sync_selection, &dir);
 		
 		if (dir == 'u')
 			ed_goto_line(ed, imax(ed->cy - line, 0));
