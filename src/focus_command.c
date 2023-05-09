cbf3e7e37c82f5cf551a33f1e94aaf6c9ddf4260
Alex Edin <lutfisk@lutfisk.net>
2023-05-09 22:07:52 +0200

    Allow multiple commands within one block

--- src/focus_command.c
+++ src/focus_command.c
@@ -43,6 +43,10 @@ struct pos {
 static global_t* ed_global = NULL;
 static editor_t* ed = NULL;
 
+static void skip_single_command(ctx_t* cx);
+static void execute_single_command(ctx_t* cx);
+static void execute(lstr_t);
+
 static
 usz parse_uint(ctx_t* cx) {
 	char* start = cx->it;
@@ -53,14 +57,16 @@ usz parse_uint(ctx_t* cx) {
 
 static
 lstr_t parse_block(ctx_t* cx) {
-	if (cx->it >= cx->end || *cx->it != '[')
+	if (cx->it >= cx->end)
 		return NLSTR();
-	++cx->it;
 
 	char* start = cx->it;
-	while (cx->it < cx->end && *++cx->it != ']')
-		;
-	return LSTR(start, cx->it - start);
+	while (cx->it < cx->end && *cx->it != '`')
+		skip_single_command(cx);
+	lstr_t block = LSTR(start, cx->it - start);
+	if (cx->it < cx->end && *cx->it == '`')
+		++cx->it;
+	return block;
 }
 
 static
@@ -72,88 +78,114 @@ pos_t parse_pos(ctx_t* cx) {
 	if (cx->it >= cx->end)
 		return pos;
 	switch (*cx->it++) {
-	case 'f':
-		pos.col += parse_uint(cx);
-		return pos;
-
-	case 'b':
-		pos.col -= parse_uint(cx);
-		return pos;
-
-	case 'u':
-		pos.line -= parse_uint(cx);
-		return pos;
-
-	case 'd':
-		pos.line += parse_uint(cx);
-		return pos;
+	case 'f': pos.col += parse_uint(cx); break;
+	case 'b': pos.col -= parse_uint(cx); break;
+	case 'u': pos.line -= parse_uint(cx); break;
+	case 'd': pos.line += parse_uint(cx); break;
 
 	case 'w':
 		if (cx->it >= cx->end)
-			return pos;
+			break;
 		switch (*cx->it++) {
-		case 'f':
-			pos.col = ed_find_word_fwd(ed);
-			return pos;
-		case 'b':
-			pos.col = ed_find_word_bwd(ed);
-			return pos;
+		case 'f': pos.col = ed_find_word_fwd(ed); break;
+		case 'b': pos.col = ed_find_word_bwd(ed); break;
 		}
 
 	case 't':
 		pos.line = 0;
 		pos.col = 0;
-		return pos;
+		break;
 
 	case 'e':
 		pos.line = ed->doc.line_count - 1;
 		pos.col = ed->doc.lines[pos.line].len;
-		return pos;
+		break;
 
 	case 's':
 		if (cx->it >= cx->end)
-			return pos;
-
+			break;
 		switch (*cx->it++) {
-		case 's':
-			ed_get_selection(ed, &pos.line, &pos.col, NULL, NULL);
-			return pos;
-
-		case 'e':
-			ed_get_selection(ed, NULL, NULL, &pos.line, &pos.col);
-			return pos;
+		case 's': ed_get_selection(ed, &pos.line, &pos.col, NULL, NULL); break;
+		case 'e': ed_get_selection(ed, NULL, NULL, &pos.line, &pos.col); break;
 		}
 		break;
 
 	case 'l':
 		if (cx->it >= cx->end)
-			return pos;
-
+			break;
 		switch (*cx->it++) {
-		case 's':
-			pos.col = 0;
-			return pos;
-
-		case 'e':
-			pos.col = ed->doc.lines[pos.line].len;
-			return pos;
-
-		default:
-			pos.line = parse_uint(cx);
-			return pos;
+		case 's': pos.col = 0; break;
+		case 'e': pos.col = ed->doc.lines[pos.line].len; break;
+		default: pos.line = parse_uint(cx); break;
 		}
 		break;
 
-	case 'i':
-		pos.col = ed_find_indent(ed);
-		return pos;
-
-	case 'c':
-		pos.col = parse_uint(cx);
-		return pos;
+	case 'i': pos.col = ed_find_indent(ed); break;
+	case 'c': pos.col = parse_uint(cx); break;
 	}
 
 	return pos;
+}
+
+static
+void skip_single_command(ctx_t* cx) {
+	switch (*cx->it++) {
+	case 'j': case 's': parse_pos(cx); break;
+	case 'c': parse_uint(cx); break;
+	case 'p': parse_uint(cx); break;
+	case 'd': break;
+	case 'l': parse_uint(cx); parse_block(cx); break;
+	case 'i': parse_block(cx); break;
+	}
+}
+
+static
+void execute_single_command(ctx_t* cx) {
+	b8 sync_selection = 0;
+
+	switch (*cx->it++) {
+	case 'j': sync_selection = 1;
+	case 's': {
+		pos_t pos = parse_pos(cx);
+		ed->cy = clamp(pos.line, 0, ed->doc.line_count - 1);
+		ed->cx = clamp(pos.col, 0, ed->doc.lines[ed->cy].len);
+		if (sync_selection)
+			ed_sync_selection(ed);
+	}	break;
+
+	case 'c': {
+		usz clipboard = parse_uint(cx);
+		if (clipboard >= CLIPBOARD_COUNT)
+			break;
+		clipboard_clear(clipboard);
+		ed_write_selection_str(ed, (lt_io_callback_t)lt_strstream_write, &clipboards[clipboard]);
+	}	break;
+
+	case 'p': {
+		usz clipboard = parse_uint(cx);
+		if (clipboard >= CLIPBOARD_COUNT)
+			break;
+		ed_insert_string(ed, clipboards[clipboard].str);
+		ed_sync_selection(ed);
+	}	break;
+
+	case 'd':
+		ed_delete_selection_if_available(ed);
+		ed_sync_selection(ed);
+		break;
+
+	case 'l': {
+		usz iterations = parse_uint(cx);
+		lstr_t block = parse_block(cx);
+		for (usz i = 0; i < iterations; ++i)
+			execute(block);
+	}	break;
+
+	case 'i':
+		ed_insert_string(ed, parse_block(cx));
+		ed_sync_selection(ed);
+		break;
+	}
 }
 
 static
@@ -162,53 +194,8 @@ void execute(lstr_t cmd) {
 	cx.it = cmd.str;
 	cx.end = cmd.str + cmd.len;
 
-	while (cx.it < cx.end) {
-		b8 sync_selection = 0;
-
-		switch (*cx.it++) {
-		case 'j': sync_selection = 1;
-		case 's': {
-			pos_t pos = parse_pos(&cx);
-			ed->cy = clamp(pos.line, 0, ed->doc.line_count - 1);
-			ed->cx = clamp(pos.col, 0, ed->doc.lines[ed->cy].len);
-			if (sync_selection)
-				ed_sync_selection(ed);
-		}	break;
-
-		case 'c': {
-			usz clipboard = parse_uint(&cx);
-			if (clipboard >= CLIPBOARD_COUNT)
-				break;
-			clipboard_clear(clipboard);
-			ed_write_selection_str(ed, (lt_io_callback_t)lt_strstream_write, &clipboards[clipboard]);
-		}	break;
-
-		case 'p': {
-			usz clipboard = parse_uint(&cx);
-			if (clipboard >= CLIPBOARD_COUNT)
-				break;
-			ed_insert_string(ed, clipboards[clipboard].str);
-			ed_sync_selection(ed);
-		}	break;
-
-		case 'd':
-			ed_delete_selection_if_available(ed);
-			ed_sync_selection(ed);
-			break;
-
-		case 'l': {
-			usz iterations = parse_uint(&cx);
-			lstr_t block = parse_block(&cx);
-			for (usz i = 0; i < iterations; ++i)
-				execute(block);
-		}	break;
-
-		case 'i':
-			ed_insert_string(ed, parse_block(&cx));
-			ed_sync_selection(ed);
-			break;
-		}
-	}
+	while (cx.it < cx.end)
+		execute_single_command(&cx);
 }
 
 void input_command(global_t* ed_global_, u32 c) {
