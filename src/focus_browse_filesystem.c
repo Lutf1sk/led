dc85bf40b2a4d21e33679f35fe07c0daf5a67c08
Alex Edin <lutfisk@lutfisk.net>
2023-06-17 05:13:06 +0200

    add filename autocompletion to filesystem browser

--- src/focus_browse_filesystem.c
+++ src/focus_browse_filesystem.c
@@ -25,16 +25,85 @@ focus_t focus_browse_filesystem = { draw_browse_filesystem, NULL, input_browse_f
 
 #define MAX_ENTRY_COUNT 15
 
-void browse_filesystem(void) {
-	focus = focus_browse_filesystem;
-	lt_led_clear(line_input);
-}
+isz selected_index = 0;
+isz visible_index = 0;
 
 typedef
 struct file {
 	lstr_t name;
-	b8 highlight;
+	b8 is_dir;
 } file_t;
+
+lt_darr(file_t) files = NULL;
+
+static
+void update_file_list(void) {
+	for (usz i = 0; i < lt_darr_count(files); ++i)
+		lt_mfree(lt_libc_heap, files[i].name.str);
+	lt_darr_clear(files);
+
+	lstr_t input = lt_led_get_str(line_input);
+
+	char dir_path_buf[PATH_MAX_LEN + 1];
+	char* dir_path = dir_path_buf;
+	memcpy(dir_path, input.str, input.len);
+	dir_path[input.len] = 0;
+
+	DIR* dir = NULL;
+	char* inp_name = dir_path;
+
+	if (input.len) {
+		inp_name += input.len - 1;
+		for (;;) {
+			if (*inp_name == '/') {
+				if (inp_name != dir_path)
+					*inp_name = 0;
+				++inp_name;
+				dir = opendir(dir_path);
+				break;
+			}
+
+			if (inp_name == dir_path) {
+				dir = opendir(".");
+				break;
+			}
+			--inp_name;
+		}
+	}
+	else
+		dir = opendir(".");
+
+	if (dir) {
+		usz inp_name_len = strlen(inp_name);
+
+		struct dirent* entry = NULL;
+		while ((entry = readdir(dir))) {
+			if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+				continue;
+			if (strncmp(entry->d_name, inp_name, inp_name_len) != 0)
+				continue;
+
+			b8 is_dir = entry->d_type == DT_DIR || entry->d_type == DT_LNK;
+			lstr_t name = lt_strdup(lt_libc_heap, lt_lstr_from_cstr(entry->d_name));
+			lt_darr_push(files, (file_t){ name, is_dir });
+		}
+
+		closedir(dir);
+	}
+
+	selected_index = lt_max_isz(lt_min_isz(selected_index, lt_darr_count(files) - 1), 0);
+	visible_index = lt_max_isz(lt_min_isz(visible_index, lt_darr_count(files) - MAX_ENTRY_COUNT), 0);
+}
+
+void browse_filesystem(void) {
+	focus = focus_browse_filesystem;
+	lt_led_clear(line_input);
+	selected_index = 0;
+
+	if (!files)
+		files = lt_darr_create(file_t, 256, lt_libc_heap);
+	update_file_list();
+}
 
 void draw_browse_filesystem(global_t* ed_globals, void* args) {
 	(void)args;
@@ -46,48 +115,6 @@ void draw_browse_filesystem(global_t* ed_globals, void* args) {
 	rec_led(line_input, clr_strs[CLR_EDITOR_SEL], clr_strs[CLR_LIST_HEAD]);
 	rec_str(" ");
 
-	lstr_t input = lt_led_get_str(line_input);
-
-	char dir_path[PATH_MAX_LEN + 1];
-	memcpy(dir_path, input.str, input.len);
-	dir_path[input.len] = 0;
-
-	char* inp_name = "";
-
-	DIR* dir = opendir(dir_path);
-	if (!dir) {
-		usz slash = 0;
-		for (usz i = 0; i < input.len; ++i)
-			if (input.str[i] == '/')
-				slash = i;
-		if (slash) {
-			dir_path[slash++] = 0;
-			dir = opendir(dir_path);
-		}
-		else
-			dir = opendir(".");
-		inp_name = &dir_path[slash];
-	}
-
-	lt_darr(file_t) files = lt_darr_create(file_t, 256, lt_libc_heap);
-
-	if (dir) {
-		usz inp_name_len = strlen(inp_name);
-
-		struct dirent* entry = NULL;
-		while ((entry = readdir(dir)) && lt_darr_count(files) < MAX_ENTRY_COUNT) {
-			if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-				continue;
-			if (strncmp(entry->d_name, inp_name, inp_name_len) != 0)
-				continue;
-
-			b8 highlight = entry->d_type == DT_DIR || entry->d_type == DT_LNK;
-			lstr_t name = lt_strdup(lt_libc_heap, lt_lstr_from_cstr(entry->d_name));
-			lt_darr_push(files, (file_t){ name, highlight });
-		}
-	}
-
-	usz visible_index = 0;
 	usz visible_count = lt_min_usz(MAX_ENTRY_COUNT, lt_darr_count(files));
 
 	for (usz i = 0; i < visible_count; ++i) {
@@ -95,7 +122,7 @@ void draw_browse_filesystem(global_t* ed_globals, void* args) {
 
 		rec_goto(2, start_height + i + 1);
 
-		if (files[index].highlight)
+		if (/*files[index].is_dir*/ index == selected_index)
 			rec_clearline(clr_strs[CLR_LIST_HIGHL]);
 		else
 			rec_clearline(clr_strs[CLR_LIST_ENTRY]);
@@ -108,33 +135,80 @@ void draw_browse_filesystem(global_t* ed_globals, void* args) {
 		rec_clearline("");
 	}
 
-	for (usz i = 0; i < lt_darr_count(files); ++i)
-		lt_mfree(lt_libc_heap, files[i].name.str);
-	lt_darr_destroy(files);
-
 	rec_crestore();
 }
 
-void input_browse_filesystem(global_t* ed_global, u32 c) {
-	editor_t* ed = ed_global->ed;
+void input_browse_filesystem(global_t* ed_globals, u32 c) {
+	editor_t* ed = ed_globals->ed;
+
+	if (c != (LT_TERM_KEY_DOWN | LT_TERM_MOD_CTRL) && c != (LT_TERM_KEY_DOWN | LT_TERM_MOD_CTRL| LT_TERM_MOD_SHIFT))
+		ed_globals->consec_cdn = 0;
+	if (c != (LT_TERM_KEY_UP | LT_TERM_MOD_CTRL) && c != (LT_TERM_KEY_UP | LT_TERM_MOD_CTRL| LT_TERM_MOD_SHIFT))
+		ed_globals->consec_cup = 0;
 
 	switch (c) {
 	case '\n': {
 		lstr_t name = lt_led_get_str(line_input);
 		if (name.len) {
-			editor_t* new_ed = fb_open(ed_global, name);
-			edit_file(ed_global, new_ed ? new_ed : ed);
+			editor_t* new_ed = fb_open(ed_globals, name);
+			edit_file(ed_globals, new_ed ? new_ed : ed);
 		}
 		else
-			edit_file(ed_global, ed);
+			edit_file(ed_globals, ed);
+	}	break;
+
+	case LT_TERM_KEY_TAB: {
+		if (!lt_darr_count(files))
+			return;
+
+		lstr_t name = files[selected_index].name;
+		usz name_offs = lt_lstr_split_bwd(lt_led_get_str(line_input), '/').len;
+		lt_led_input_str(line_input, LSTR(name.str + name_offs, name.len - name_offs));
+		if (files[selected_index].is_dir)
+			input_term_key(line_input, '/');
+		update_file_list();
+	}	break;
+
+	case LT_TERM_KEY_UP:
+		if (selected_index) {
+			if (--selected_index < visible_index)
+				--visible_index;
+		}
+		break;
+	case LT_TERM_KEY_DOWN:
+		if (selected_index < lt_darr_count(files) - 1) {
+			if (++selected_index >= visible_index + MAX_ENTRY_COUNT)
+				++visible_index;
+		}
+		break;
+
+	case LT_TERM_KEY_UP | LT_TERM_MOD_CTRL: {
+		usz vstep = ++ed_globals->consec_cup * ed_globals->vstep;
+		for (usz i = 0; i < vstep; ++i) {
+			if (selected_index) {
+				if (--selected_index < visible_index)
+					--visible_index;
+			}
+		}
+	}	break;
+
+	case LT_TERM_KEY_DOWN | LT_TERM_MOD_CTRL: {
+		usz vstep = ++ed_globals->consec_cdn * ed_globals->vstep;
+		for (usz i = 0; i < vstep; ++i) {
+			if (selected_index < lt_darr_count(files) - 1) {
+				if (++selected_index >= visible_index + MAX_ENTRY_COUNT)
+					++visible_index;
+			}
+		}
 	}	break;
 
 	case LT_TERM_KEY_BSPACE: case LT_TERM_KEY_BSPACE | LT_TERM_MOD_CTRL:
 		if (!lt_darr_count(line_input->str))
 	case LT_TERM_KEY_ESC:
-			edit_file(ed_global, ed);
+			edit_file(ed_globals, ed);
 	default:
-		input_term_key(line_input, c);
+		if (input_term_key(line_input, c))
+			update_file_list();
 		break;
 	}
 }
